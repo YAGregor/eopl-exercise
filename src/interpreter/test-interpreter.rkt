4128588f67c58eb26881759ba676adf77b41fbb7
yagregor <yagregor@qq.com>
2023-08-15 15:44:08 +0800

    feat: test for if

--- src/interpreter/test-interpreter.rkt
+++ src/interpreter/test-interpreter.rkt
@@ -4,10 +4,14 @@
 (define basic-let "let x = 1 in x")
 (define multi-branch-let "let x = 1 y = 2 in x")
 (define test-opreration "let x = 1 y = 1 in list(x,y)")
+(define test-if "let x = 1 in if zero?(x) then 2 else 3")
 
 (define interpreter-tests
   (test-suite
    "tests for eopl interpreter"
+   (test-case 
+   "if"
+   (check-equal? 3 (run test-if)))
    (test-case
    "operation call"
    (check-equal? (exp-list-pair-value (run test-opreration)) 1))
